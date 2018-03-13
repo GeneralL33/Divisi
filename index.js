8d194ddca558878f1d709ee77850ceea5e5eed17
Jeremy Cantu <mail@jeremycantu.com>
2018-03-13 15:06:29 -0500

    Socket IO chat example boilerplate

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+/********* Socket.IO concerns *********/
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+/***************************************/
+
+http.listen(3005, function () {
+    console.log('listening on *:3005');
+});
