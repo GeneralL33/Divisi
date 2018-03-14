f1a17c7e8e074453a542a3853b1e5876efb07fbb
Jeremy Cantu <mail@jeremycantu.com>
2018-03-13 21:55:23 -0500

    nconf package installation and usage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var app = require('express')(),
+var nconf = require('nconf'),
+    app = require('express')(),
     http = require('http').Server(app),
     SerialPort = require('serialport'),
     io = require('socket.io')(http);
@@ -7,19 +8,41 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+/********* Environment configuration concerns *********/
+// First consider commandline arguments and environment variables, respectively.
+nconf.argv().env();
+
+// Then load configuration from a designated file.
+nconf.file({
+    file: 'config.json'
+});
+
+// Provide default values for settings not provided above.
+nconf.defaults({
+    'http': {
+        'port': 3005
+    },
+    'serialPort': {
+        'port': 'COM4',
+        'baudRate': 9600
+    }
+});
+
 /********* SerialPort concerns *********/
 const Readline = SerialPort.parsers.Readline;
 
 // port subject to change!
-var port = new SerialPort('COM3', {
-    baudRate: 9600
+var port = new SerialPort(nconf.get('serialPort:port'), {
+    baudRate: nconf.get('serialPort:baudRate')
 }, (err) => {
     if (err) {
         return console.log('Error: ', err.message);
     }
 });
 
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+const parser = port.pipe(new Readline({
+    delimiter: '\r\n'
+}));
 
 port.on('open', () => {
     // Server is connected to Arduino
@@ -50,7 +73,7 @@ io.on('connection', (socket) => {
 /***************************************/
 
 // Listen on port
-app.set('port', (process.env.PORT || 3005));
+app.set('port', (process.env.PORT || nconf.get('http:port') || 3005));
 
 // Notfy port
 http.listen(app.get('port'), () => {
