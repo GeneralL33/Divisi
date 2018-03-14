0eb835412a1b504517014718dd5436f760536d6a
GeneralL33 <31070212+GeneralL33@users.noreply.github.com>
2018-03-14 04:57:47 -0500

    updates

--- ArduinoCode/Divisi/Divisi_version_2/Divisi_version_2.ino/Divisi_version_2.ino.ino
+++ ArduinoCode/Divisi/Divisi_version_2/Divisi_version_2.ino/Divisi_version_2.ino.ino
@@ -16,8 +16,11 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(90, PIN, NEO_GRB + NEO_KHZ800);
 
 int sensorPin = A0; // select the input pin for the potentiometer
-int sensorValue = 0; // variable to store the value coming from the sensor
 int digPin = 7;
+int kickValue = 0; // variable to store the value coming from the sensor
+int snareValue = 0; // variable to store the value coming from the sensor
+int tom1Value = 0; // variable to store the value coming from the sensor
+int tom2Value = 0; // variable to store the value coming from the sensor
 boolean val = 0;
  
 void setup () 
@@ -30,18 +33,48 @@ void setup ()
  
 void loop () 
 {
-  sensorValue = analogRead (sensorPin);
+  kickValue = analogRead (sensorPin);
+  snareValue = analogRead (sensorPin);
+  tom1Value = analogRead (sensorPin);
+  tom2Value = analogRead (sensorPin);
   val = digitalRead(digPin);
   Serial.print("Digital Value: ");
   Serial.println(val);
   Serial.print("Analog Value: ");
-  Serial.println (sensorValue, DEC);
-  colorWipe(strip.Color(sensorValue, 0, 0), 60);
+  Serial.println (kickValue, DEC);
+  kickWipe(strip.Color(kickValue, 0, 0), 60);
+  snareWipe(strip.Color(0, snareValue, 0), 60);
+  tom1Wipe(strip.Color(0, 0, tom1Value), 60);
+  tom2Wipe(strip.Color(tom2Value, tom2Value, tom2Value), 60);
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
+void kickWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i+=4) {
+    strip.setPixelColor(i, c);
+    strip.show();
+  }
+}
+
+// Fill the dots one after the other with a color
+void snareWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=1; i<strip.numPixels(); i+=4) {
+    strip.setPixelColor(i, c);
+    strip.show();
+  }
+}
+
+// Fill the dots one after the other with a color
+void tom1Wipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=2; i<strip.numPixels(); i+=4) {
+    strip.setPixelColor(i, c);
+    strip.show();
+  }
+}
+
+// Fill the dots one after the other with a color
+void tom2Wipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=3; i<strip.numPixels(); i+=4) {
     strip.setPixelColor(i, c);
     strip.show();
   }
